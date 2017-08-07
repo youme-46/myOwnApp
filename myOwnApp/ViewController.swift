425c39b1df3bf3118cf346545517929970c1ec47
youme-46 <youme-46@users.noreply.github.com>
2017-08-07 15:17:08 +0900

    Initial Commit

---
+++ myOwnApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myOwnApp
+//
+//  Created by 畑中佑太 on 2017/08/07.
+//  Copyright © 2017年 myOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
